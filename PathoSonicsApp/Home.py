b7601f5ff6b7c6c2eea2b11bb4b00f8af98901ec
adriannecheng <chengadrianne@gmail.com>
2026-06-14 16:11:40 -0700

    Update Home.py

--- PathoSonicsApp/Home.py
+++ PathoSonicsApp/Home.py
@@ -183,4 +183,4 @@ with nav_col3:
         st.rerun()
 
 st.markdown("---")
-st.info("💡 *Want to process custom data, or trying it for yourself? Navigate to the **Explore** page inside your left sidebar menu panel!*")
+st.info("💡 *Want to process custom data, or try it for yourself? Navigate to the **Explore** page inside your left sidebar menu panel!*")
